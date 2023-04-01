cc07831089f543f21cd73b09d05612bfa23f16a8
Alexander Bortsov <A.bortsovv@yandex.ru>
2023-04-01 20:26:45 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         int [] week = new int[3];
+         week[0] = 1234;
+         week[1] = 42341;
+         week[2] = 24421;
+
+       SalesManager salesManager = new SalesManager(week);
+       System.out.println(salesManager.max());
     }
 }

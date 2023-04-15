ace72b20d961d832ab347764fd660678326f49db
Alexander Bortsov <A.bortsovv@yandex.ru>
2023-04-15 18:42:34 +0300

    ModifiedHomework1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-         int [] week = new int[3];
-         week[0] = 1234;
-         week[1] = 42341;
-         week[2] = 24421;
+         int [] week = new int[6];
+         week[0] = 12;
+         week[1] = 42;
+         week[2] = 244;
+         week[3] = 4;
+         week[4] = 123;
+         week[5] = 12;
+
 
        SalesManager salesManager = new SalesManager(week);
        System.out.println(salesManager.max());

570cf37f1fe360f1e1dc332f08ad0e87362ee2ee
Alexander Bortsov <A.bortsovv@yandex.ru>
2023-04-15 19:44:34 +0300

    StatModified

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
          int [] week = new int[6];
          week[0] = 12;
@@ -15,7 +14,5 @@ public class Main {
 
        SalesManager salesManager = new SalesManager(week);
        System.out.println(salesManager.max());
-       AveregeIncome averegeIncome = new AveregeIncome(week);
-       System.out.println(averegeIncome.average());
     }
 }

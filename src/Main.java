46f175617a6712fa415f7ac71c1d4b3e141dc76d
Alexander Bortsov <A.bortsovv@yandex.ru>
2023-04-15 20:13:26 +0300

    Stat2.0

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,6 @@ public class Main {
        SalesManager salesManager = new SalesManager(week);
        System.out.println(salesManager.max());
        System.out.println(salesManager.average());
+       System.out.println(salesManager.average());
     }
 }
